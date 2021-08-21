f7c6b4cb55f0837a0c098826ded2e58408b35bcd
Carter-O <carterannandale@gmail.com>
2021-08-21 11:50:53 +0100

    initial commit

---
+++ src/main/java/me/carter/mcting/mcting/JuulCraft.java
@@ -0,0 +1,96 @@
+package me.carter.mcting.mcting;
+
+import jdk.tools.jlink.internal.plugins.StringSharingPlugin;
+import me.carter.mcting.mcting.events.PlayerUseJuul;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionData;
+import org.bukkit.potion.PotionType;
+import org.bukkit.util.io.BukkitObjectInputStream;
+
+import java.util.ArrayList;
+
+public final class JuulCraft extends JavaPlugin {
+    public NamespacedKey key = new NamespacedKey(this, "dur");
+
+    @Override
+    public void onEnable() {
+        // Juul Pod Recipe
+
+        ItemStack juulPod = new ItemStack(Material.SUGAR, 1);
+        ItemMeta juulMeta = juulPod.getItemMeta();
+
+        ArrayList<String> lore = new ArrayList();
+        lore.add("May Contain Carcinogens");
+        juulMeta.setDisplayName("Juul Pod");
+        juulMeta.addEnchant(Enchantment.PIERCING, 10, true);
+        juulMeta.setLore(lore);
+
+        juulPod.setItemMeta(juulMeta);
+
+        ShapelessRecipe juulPodRecipe = new ShapelessRecipe(juulPod);
+        juulPodRecipe.addIngredient(Material.BLACKSTONE);
+
+        // Empty Juul -> Juul Recipe
+
+        ItemStack bottle = new ItemStack(Material.POTION, 1);
+        PotionMeta bottleMeta = (PotionMeta) bottle.getItemMeta();
+
+        bottleMeta.setDisplayName("Juul");
+        bottleMeta.addEnchant(Enchantment.PIERCING, 10, true);
+        bottleMeta.setBasePotionData(new PotionData(PotionType.WATER));
+
+        bottle.setItemMeta(bottleMeta);
+
+        ShapelessRecipe bottleRecipe = new ShapelessRecipe(bottle);
+
+        ItemStack brokenJuul = new ItemStack(Material.GLASS_BOTTLE);
+        ItemMeta brokenMeta = brokenJuul.getItemMeta();
+        brokenMeta.addEnchant(Enchantment.PIERCING, 10, true);
+        brokenMeta.setDisplayName("Empty Juul");
+
+        brokenJuul.setItemMeta(brokenMeta);
+
+        bottleRecipe.addIngredient(new RecipeChoice.ExactChoice(brokenJuul));
+        bottleRecipe.addIngredient(new RecipeChoice.ExactChoice(juulPod));
+
+        // Juul Recipe
+
+        ItemStack bottle1 = new ItemStack(Material.POTION, 1);
+        PotionMeta bottleMeta1 = (PotionMeta) bottle1.getItemMeta();
+
+        bottleMeta1.setDisplayName("Juul");
+        bottleMeta1.addEnchant(Enchantment.PIERCING, 10, true);
+        bottleMeta1.setBasePotionData(new PotionData(PotionType.WATER));
+
+        bottle1.setItemMeta(bottleMeta1);
+
+        ShapedRecipe bottleRecipe1 = new ShapedRecipe(bottle1);
+        bottleRecipe1.shape(" % "," B "," % ");
+
+        bottleRecipe1.setIngredient('%', Material.BLACKSTONE);
+        bottleRecipe1.setIngredient('B', Material.GLASS_BOTTLE);
+
+        // Registering Events / Recipes
+
+        Bukkit.getServer().addRecipe(bottleRecipe);
+        Bukkit.getServer().addRecipe(bottleRecipe1);
+        Bukkit.getServer().addRecipe(juulPodRecipe);
+
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerUseJuul(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/carter/mcting/mcting/events/PlayerUseJuul.java
@@ -0,0 +1,78 @@
+package me.carter.mcting.mcting.events;
+
+import me.carter.mcting.mcting.JuulCraft;
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.util.Vector;
+
+public class PlayerUseJuul implements Listener {
+    private JuulCraft mainClass = JuulCraft.getPlugin(JuulCraft.class);
+
+    @EventHandler
+    public void onPlayerUseJuul(PlayerItemConsumeEvent e) {
+        if (e.getItem().getItemMeta().getDisplayName().equals("Juul")) {
+            Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(255, 255, 255), 2.0F);
+            e.getPlayer().spawnParticle(Particle.REDSTONE, e.getPlayer().getLocation().add(new Vector(0, 1.7, 0)), 3000, dustOptions);
+            e.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE+"You have taken a hit of the Juul!");
+
+            ItemStack newItemForData = e.getItem();
+            PersistentDataContainer persistentDataContainer = newItemForData.getItemMeta().getPersistentDataContainer();
+
+            if (persistentDataContainer.get(mainClass.key, PersistentDataType.INTEGER) != null) {
+                int dur = persistentDataContainer.get(mainClass.key, PersistentDataType.INTEGER);
+                Bukkit.getServer().broadcastMessage(String.valueOf(dur));
+
+                ItemStack newItemA = e.getItem();
+                ItemMeta newMeta = newItemA.getItemMeta();
+                newMeta.getPersistentDataContainer().set(mainClass.key, PersistentDataType.INTEGER, dur + 1);
+                newItemA.setItemMeta(newMeta);
+
+                e.setItem(newItemA);
+
+                if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.POTION) {
+                    e.getPlayer().getInventory().setItemInMainHand(newItemA);
+                } else if (e.getPlayer().getInventory().getItemInOffHand().getType() == Material.POTION) {
+                    e.getPlayer().getInventory().setItemInOffHand(newItemA);
+                }
+                
+                if ((dur + 1) > 9) {
+                    ItemStack brokenJuul = new ItemStack(Material.GLASS_BOTTLE);
+                    ItemMeta brokenMeta = brokenJuul.getItemMeta();
+                    brokenMeta.addEnchant(Enchantment.PIERCING, 10, true);
+                    brokenMeta.setDisplayName("Empty Juul");
+
+                    brokenJuul.setItemMeta(brokenMeta);
+
+                    if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.POTION) {
+                        e.getPlayer().getInventory().setItemInMainHand(brokenJuul);
+                    } else if (e.getPlayer().getInventory().getItemInOffHand().getType() == Material.POTION) {
+                        e.getPlayer().getInventory().setItemInOffHand(brokenJuul);
+                    }
+                    e.getPlayer().sendMessage(ChatColor.RED+"Oh No! You ran out of Juul Pods");
+                }
+            } else {
+                Bukkit.getServer().broadcastMessage("adding now...?");
+                ItemStack ActualNewItem = e.getItem();
+
+                ItemMeta newItem = ActualNewItem.getItemMeta();
+                newItem.getPersistentDataContainer().set(mainClass.key, PersistentDataType.INTEGER, 1);
+                ActualNewItem.setItemMeta(newItem);
+
+                e.setItem(ActualNewItem);
+                if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.POTION) {
+                    e.getPlayer().getInventory().setItemInMainHand(ActualNewItem);
+                } else if (e.getPlayer().getInventory().getItemInOffHand().getType() == Material.POTION) {
+                    e.getPlayer().getInventory().setItemInOffHand(ActualNewItem);
+                }
+            }
+            e.setCancelled(true);
+        }
+    }
+}
